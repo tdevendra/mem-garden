f9bdd3144dbd5068566d9c2389bb000ad39928ac
Devendra Tapdia <devendratapdia@gmail.com>
2017-05-19 12:09:35 +0000

    working

---
+++ final/test.py
@@ -0,0 +1,55 @@
+import re
+import memory_profiler
+import time
+import sys
+
+d = {}
+def read_in_chunks(f, size=1024):
+    while True:
+        chunk = f.read(size)
+        if not chunk:
+            break
+        yield chunk
+
+def read_in_chunks2(f, size=1024):
+    chunk = f.read(size)
+    if not chunk:
+        return
+    yield chunk
+
+def calc_words(word):
+    word = word.lower()
+    if re.search("[A-Za-z0-9]+", word):
+        pass
+    else:
+        return
+
+    if word in d:
+        d[word] = d[word] + 1
+    else:
+        d[word] = 1
+
+def process_chunk(data):
+    print("In process chunk")
+    lines = data.split("\n")
+    for line in lines:
+        words = line.split()
+        words = map(lambda y: y.strip(), words)        
+        words = map(calc_words, words)
+
+print("Prof Memory before {} Mb".format(memory_profiler.memory_usage()))
+print("Memory before {} Mb".format(sys.getsizeof(d)))
+t1 = time.clock()   
+with open('testin.txt') as f:
+    for chunk in f:
+        process_chunk(chunk)
+
+t2 = time.clock()
+        
+for k, v in d.items():
+    print ("Word: " + str(k) + " Count: " + str(v))
+
+print("Memory after {} Mb".format(sys.getsizeof(d)))
+
+print("Prof Memory after {} Mb".format(memory_profiler.memory_usage()))
+
